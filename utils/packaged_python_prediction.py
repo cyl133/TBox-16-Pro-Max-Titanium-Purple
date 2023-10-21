eb9e16c107c4bcfffa23266e11d2acf7744f9416
cyli <00lcylcy00@gmail.com>
2023-10-21 14:16:51 -0400

    adds python script for nodejs server to call

---
+++ utils/packaged_python_prediction.py
@@ -0,0 +1,25 @@
+import sys
+from complexity import complexity_prediction, NewData
+
+def main(title, description, stress):
+    # Create a NewData object with the command-line arguments
+    issue_data = NewData(title=title, description=description, stress=stress)
+
+    # Get the prediction response using the complexity_prediction function
+    response = complexity_prediction(issue_data)
+
+    # Print the response so that the calling process (Node.js server) can read it
+    print(response)
+
+# Entry point of the script
+if __name__ == "__main__":
+    # Check to ensure proper command-line arguments have been passed
+    if len(sys.argv) < 4:
+        print("Error: Not enough arguments provided.")
+        sys.exit(1)
+
+    # Unpack the command-line arguments into variables
+    _, title, description, stress = sys.argv  # The first argument is the script name, so we disregard it
+
+    # Call the main function with the command-line arguments
+    main(title, description, stress)
