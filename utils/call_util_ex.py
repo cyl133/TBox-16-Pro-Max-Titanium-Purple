66b2592454a5175006c7186958cbff3ac83c4acd
NghiaNim <ntn6039@nyu.edu>
2023-10-21 12:53:37 -0400

    added json response example

--- utils/call_util_ex.py
+++ utils/call_util_ex.py
@@ -5,7 +5,7 @@ json_string = '{"title": "page is not loading", "description": "page is stuck on
 data = NewData.parse_raw(json_string)
 
 # Test the function with a sample NewData object
-test_data = NewData(title="page is not responding", description="The page freezes every time I try to submit the form.", stress="high stress")
+# test_data = NewData(title="page is not responding", description="The page freezes every time I try to submit the form.", stress="high stress")
 
 response = complexity_prediction(data)
 
