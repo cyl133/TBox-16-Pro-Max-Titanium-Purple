0c962bc4e7bd030c27bcc4bf81517efc566e318e
Juan Guevara <juandi.diego.7@gmail.com>
2023-10-22 01:12:06 -0400

    calculate moving average for time prediciton

--- issue_puller/github.py
+++ issue_puller/github.py
@@ -17,59 +17,58 @@ access_token = os.getenv("GITHUB_API")
 base_url = 'https://api.github.com'
 
 stress = ['very stressed', 'somewhat stress', 'relaxed', 'unbothered']
-columns = ['Title', 'Description', 'Time', 'Stress']
+columns = ['Title', 'Description', 'Time']
 
 # Retrieve issues from a repository
 github_username = 'your-github-username'
-repository_owner = 'langchain-ai'
-repository_name = 'langchain'
+
 
 #event: issue or pull
-def get_data(event):
+def get_data(repository_owner, repository_name, event):
     endpoint = f'/repos/{repository_owner}/{repository_name}/{event}'
     headers = {'Authorization': f'Bearer {access_token}'}
     response = requests.get(base_url + endpoint, headers=headers)
     if response.status_code == 200:
         issues = response.json()
-        for issue in issues:
-            print(f"Issue #{issue['number']}: {issue['title']}")
+        # for issue in issues:
+        #     print(f"Issue #{issue['number']}: {issue['title']}")
     else:
         print(f"Failed to retrieve issues. Status code: {response.status_code}")
     return issues
 
 def unix_timestamp(string):
     return int(dp.parse(string).timestamp())
-    
-def rand_stress():
-    return stress[random.randint(0, 3)]
      
 def processed_datum(datapoint):
     processed_datapoint = [datapoint['title'], datapoint['body']] 
     time_taken = unix_timestamp(datapoint['closed_at']) - unix_timestamp(datapoint['created_at'])
     processed_datapoint.append(time_taken)
-    processed_datapoint.append(rand_stress())
     return processed_datapoint
 
-def get_complexity(data):
+def get_rolling_avg(data):
     df = pd.DataFrame(data, columns=columns)
+    df['Time'] = df['Time']/60
     df.sort_values(by='Time', ascending=False, inplace=True)
-    top_20_percentile = int(0.2 * len(df))
-    next_50_percentile = int(0.7 * len(df))
+    top_third = int(0.33 * len(df))
+    next_third = int(0.66 * len(df))
     df['Category'] = 'Easy'
-    df.loc[df.index[:top_20_percentile], 'Category'] = 'Hard'
-    df.loc[df.index[top_20_percentile:next_50_percentile], 'Category'] = 'Medium'
-    return df
+    df.loc[df.index[:top_third], 'Category'] = 'Hard'
+    df.loc[df.index[top_third:next_third], 'Category'] = 'Medium'
     
+    easy_avg = df[df['Category'] == 'Easy']['Time'].mean()
+    medium_avg = df[df['Category'] == 'Medium']['Time'].mean()
+    hard_avg = df[df['Category'] == 'Hard']['Time'].mean()
+
+    return [easy_avg, medium_avg, hard_avg]
     
-data = get_data('issues?state=closed')
+def github_time_taken(repository_name, repository_owner):  
+    data = get_data(repository_name, repository_owner, 'issues?state=closed')
+    processed_data = [processed_datum(datum) for datum in data]
+    avgs = get_rolling_avg(processed_data)
+    print(avgs)
 
-processed_data = [processed_datum(datum) for datum in data]
-
-sub = get_complexity(processed_data)
-
-sub.to_csv('langchain_issue.csv', index=False)
-
-
+if __name__ == '__main__':
+    github_time_taken('langchain-ai', 'langchain')
 
 
 
