a6b87cf79a481b15f84dc054defe6c2f286bf276
NghiaNim <ntn6039@nyu.edu>
2023-10-22 02:06:39 -0400

    changed the UI to make it look nicer for launch page

--- electron-vite/src/renderer/src/pages/launch.tsx
+++ electron-vite/src/renderer/src/pages/launch.tsx
@@ -17,31 +17,38 @@ const Launch = (): JSX.Element => {
         style={{
           fontWeight: 500,
           fontSize: '56px',
-          alignSelf: 'center',
           paddingBottom: '17px',
-          color: 'black'
+          color: 'black',
+          textAlign: 'center'
         }}
       >
-        TBox 16 ProMax
+        TBox 16 Pro Max
       </h1>
-      <div style={{ paddingBottom: '52px', alignItems: 'center' }}>
-        IAmSpeed applies LLMs to solve time boxing and time estimation{' '}
+      <div style={{ paddingBottom: '52px', alignItems: 'center', textAlign: 'center' }}>
+        Our TBox 16 Pro Max (titanium purple) applies LLMs to solve time boxing and time estimation{' '}
       </div>
       <Link
         to="/gettingStarted"
         style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
           height: '49px',
           color: 'white',
           fontWeight: 'bold',
           fontSize: '16px',
           backgroundColor: '#7CADB9',
           width: '300px',
-          marginBottom: '18px'
+          marginBottom: '18px',
+          textDecoration: 'none',  // Removes the underline from the link
+          border: 'none',
+          borderRadius: '4px'  // Optional: to give it a rounded button look
         }}
       >
-        <img src="./github.svg" alt="Github" />
-        <button>Sign Up With GitHub</button>
+        Sign Up With GitHub
+        
       </Link>
+
       <div style={{ fontWeight: 500, fontSize: '13px' }}>Already using TBox 16 ProMax</div>
       <div style={{ fontWeight: 'bold', textDecoration: 'underline', fontSize: '13px' }}>
         Sign in using Github
